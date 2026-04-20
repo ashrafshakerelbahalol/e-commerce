e70d8e180346fc6f7c0dd6b4835937e88fcfc222
ashrafshakerelbahalol <93045464+ashrafshakerelbahalol@users.noreply.github.com>
2026-04-20 07:44:46 +0200

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,12 +1,18 @@
 -- Write an SQL query to generate a daily report of the total revenue for a specific date.
 select count(*) as count , sum(total_amount) As 'total amount', Date(order_date) As date_of_day 
 from order_products
-group by date(order_date);
+WHERE Date(order_date) = '2026-04-10'
+group by date_of_day ;
 
 -- * Write an SQL query to generate a monthly report of the top-selling products in a given month.
-select count(*) as count , sum(total_amount) As 'total amount',  DATE_FORMAT(order_date, '%M %Y') AS 'month' 
-from order_products
-group by DATE_FORMAT(order_date, '%M %Y');
+select sum(d.quantity) as count , p.name ,  p.product_id ,sum((d.quantity)*d.unit_price) AS 'total price'
+from order_detials d inner join product p
+on d.product_id= p.product_id
+inner join order_products o 
+on d.order_id = o.order_id
+WHERE o.order_date >= '2026-03-01' AND o.order_date < '2026-04-01'
+group by product_id,name
+order by count desc ;
 
 -- Write a SQL query to retrieve a list of customers who have placed orders totaling more than $500 in the past month. 
 -- Include customer names and their total order amounts.
